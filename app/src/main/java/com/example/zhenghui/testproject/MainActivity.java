d1a809147cdd76284535f193a8a72755c52c50f3
zhenghui <344505560@qq.com>
2016-02-21 17:03:54 +0800

    textview commit

--- app/src/main/java/com/example/zhenghui/testproject/MainActivity.java
+++ app/src/main/java/com/example/zhenghui/testproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView TextView = new TextView(getApplicationContext());
     }
 
     @Override
